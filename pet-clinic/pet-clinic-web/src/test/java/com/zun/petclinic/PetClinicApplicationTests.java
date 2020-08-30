231f3587fd25b52fc20f72e75aec293d5ebe9f8c
Zun <zunsamuel97@gmail.com>
2020-08-30 16:18:17 -0500

    working on mvn release:prepare

--- pet-clinic/pet-clinic-web/src/test/java/com/zun/petclinic/PetClinicApplicationTests.java
+++ pet-clinic/pet-clinic-web/src/test/java/com/zun/petclinic/PetClinicApplicationTests.java
@@ -1,4 +1,4 @@
-package petclinic;
+package com.zun.petclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PetClinicApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        System.out.println("banan");
+    }
 
 }
